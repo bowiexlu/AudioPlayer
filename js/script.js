dc933374a98fe5f4bbcbaed248ba70040d2ea327
lu <boeylv@gmail.com>
2024-10-17 18:42:21 +0300

    upload folder

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const songList = [
+	{
+	name: "Fade",
+	artist: "Alan Walker",
+	imageSrc: "bild1.jpg",
+	soundSrc: "Alan Walker - Fade.mp3"
+	},
+	{
+	name: "Arc",
+	artist: "NCS",
+	imageSrc: "bild2.jpg",
+	soundSrc: "NCS - Ark.mp3"
+	},
+	{
+	name: "Weapon",
+	artist: "M4SONIC",
+	imageSrc: "bild3.jpg",
+	soundSrc: "M4SONIC - Weapon.mp3"
+	},
+	
+	];
+
