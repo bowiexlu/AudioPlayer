9f438dabc9ff7657f8f8cbe99df0bab832febb0f
lu <boeylv@gmail.com>
2024-10-19 19:42:12 +0300

    edit path address

--- js/script.js
+++ js/script.js
@@ -2,22 +2,21 @@ const songList = [
 	{
 	name: "Fade",
 	artist: "Alan Walker",
-	imageSrc: "bild1.jpg",
-	soundSrc: "Alan Walker - Fade.mp3"
+	imageSrc: "img/bild1.jpg",
+	soundSrc: "audio/Alan Walker - Fade.mp3"
 	},
 	{
 	name: "Arc",
 	artist: "NCS",
-	imageSrc: "bild2.jpg",
-	soundSrc: "NCS - Ark.mp3"
+	imageSrc: "img/bild2.jpg",
+	soundSrc: "audio/NCS - Ark.mp3"
 	},
 	{
 	name: "Weapon",
 	artist: "M4SONIC",
-	imageSrc: "bild3.jpg",
-	soundSrc: "M4SONIC - Weapon.mp3"
+	imageSrc: "img/bild3.jpg",
+	soundSrc: "audio/M4SONIC - Weapon.mp3"
 	},
-	
 ];
 
 let currentSongIndex = 0;
